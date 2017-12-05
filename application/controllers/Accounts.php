47685154c1f2f8196a98d79c0ac6c248be785e1e
amaceh <achmadar9i@gmail.com>
2017-12-05 16:58:17 +0700

    initial commit

---
+++ application/controllers/Accounts.php
@@ -0,0 +1,65 @@
+<?php
+ 
+require APPPATH . '/libraries/REST_Controller.php';
+ 
+class Accounts extends REST_Controller {
+ 
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+    }
+ 
+    // show data mahasiswa
+    function index_get() {
+        $username = $this->get('username');
+        if ($username == '') {
+            $users = $this->db->get('accounts')->result();
+        } else {
+            $this->db->where('username', $username);
+            $users = $this->db->get('accounts')->result();
+        }
+        $this->response($users, 200);
+    }
+ 
+    // insert new data to mahasiswa
+    function index_post() {
+        $data = array(
+                    'username'      => $this->post('username'),
+                    'password'          => $this->post('password'),
+                    'email'       => $this->post('email'));
+        $insert = $this->db->insert('accounts', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+ 
+    // update data mahasiswa
+    function index_put() {
+        $username = $this->put('username');
+        $data = array(
+                    'username'      => $this->put('username'),
+                    'password'          => $this->put('password'),
+                    'email'       => $this->put('email'));
+        $this->db->where('username', $username);
+        $update = $this->db->update('accounts', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+ 
+    // delete mahasiswa
+    function index_delete() {
+        $username = $this->delete('username');
+        $this->db->where('username', $username);
+        $delete = $this->db->delete('accounts');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+ 
+}
